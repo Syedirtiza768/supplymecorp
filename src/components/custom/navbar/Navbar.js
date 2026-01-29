96d0323346ee39e17d892a86d04e81504be49af0
Syedirtiza768 <m.mudasirasfi@gmail.com>
2026-01-29 19:46:47 +0500

    Update Shop navigation link from Building to Tools category

--- src/components/custom/navbar/Navbar.js
+++ src/components/custom/navbar/Navbar.js
@@ -83,7 +83,7 @@ const Navbar = () => {
             <SimpleNavbarItem
               mobileNavbar={mobileNavbar}
               title="Shop"
-              link="/shop?category=Building&page=1&limit=10&sortBy=id&sortOrder=DESC"
+              link="/shop?category=Tools&page=1&limit=10&sortBy=id&sortOrder=DESC"
             />
             <SimpleNavbarItem
               mobileNavbar={mobileNavbar}
